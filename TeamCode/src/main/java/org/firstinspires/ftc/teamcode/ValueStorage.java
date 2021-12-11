990a09f8050ad57996197f1fe964669c8863fc58
Rishi Rane <hrishi.rane@gmail.com>
2021-12-11 11:06:15 -0600

    Tested and working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ValueStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ValueStorage.java
@@ -1,16 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 public class ValueStorage {
-    public static final double armRest = 0.43;
-    public static final double armDown = 0.795;
-    public static final double armUp = 0.65;
+    public static final double armRest = 0.20;
+    public static final double armDown = 0.50;
+    public static final double armUp = 0.30;
     public static final double clawOpen = 0.43;
     public static final double clawClosed = 0.76;
     public static final double bucketRest = 0.36;
     public static final double bucketUp = 0.58;
     public static final double bucketDown = 0.80;
-    public static final double gateDown = 0.31;
-    public static final double gateUp = 0.65;
+    public static final double gateDown = 0.35;
+    public static final double gateUp = 0.58;
     public static final double bucketSensorThreshold = 4.5;
     public static final double[] armStateTimes = {1000, 1000, 1000, 1000, 1000, 1000};
     public static final double[][] intakeStateTimes = {{500, 750, 1000}, {250, 250, 250}, {250, 250, 500}, {250, 250, 250}};
